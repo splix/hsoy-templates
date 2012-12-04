e0f3abb8be9658a5fc6785a8a4c9bfa70d0e9004
Igor Artamonov <igor@artamonov.ru>
2012-12-04 16:45:05 +0700

    utility methods for JS Compiler

--- src/main/java/com/the6hours/hsoytemplates/HsoyJsCompiler.java
+++ src/main/java/com/the6hours/hsoytemplates/HsoyJsCompiler.java
@@ -1,6 +1,8 @@
 package com.the6hours.hsoytemplates;
 
 import com.google.template.soy.SoyFileSet;
+import com.google.template.soy.base.SoySyntaxException;
+import com.google.template.soy.jssrc.SoyJsSrcOptions;
 
 import java.io.*;
 import java.util.List;
@@ -10,6 +12,27 @@ import java.util.List;
  * @since 23.09.12
  */
 public class HsoyJsCompiler {
+
+    public String compileToString(String input) throws HsoyFormatException, SoySyntaxException {
+        return compileToString(input, "unknown.hsoy");
+    }
+
+    public String compileToString(String input, String name) throws HsoyFormatException, SoySyntaxException {
+        HsoyParser parser = new HsoyParser();
+        HsoyDocument document = parser.parse(input);
+        String soy = document.getSoy();
+        SoyFileSet.Builder sfs = new SoyFileSet.Builder();
+        sfs.add(soy, name);
+
+        SoyFileSet soyFileSet = sfs.build();
+
+        SoyJsSrcOptions jsSrcOptions = new SoyJsSrcOptions();
+        jsSrcOptions.setShouldProvideRequireSoyNamespaces(false);
+        jsSrcOptions.setShouldGenerateJsdoc(false);
+        List<String> compiledSrcs = soyFileSet.compileToJsSrc(jsSrcOptions, null);
+        assert compiledSrcs.size() == 1;
+        return compiledSrcs.get(0);
+    }
 
     public SoyFileSet build(List<File> files) throws HsoyFormatException, IOException {
         HsoyParser parser = new HsoyParser();
